1238e21dcfb91ca60e19e7b7f183dea9a9a153d9
Hannah Mandell <hfmandell@gmail.com>
2023-03-28 00:25:34 -0700

    uncommented normalized

--- load_tweets.sh
+++ load_tweets.sh
@@ -7,11 +7,11 @@ files='
 test-data.zip
 '
 
-#echo 'load normalized'
-#for file in $files; do
+echo 'load normalized'
+for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
-#    python3 load_tweets.py --db="postgresql://postgres:pass@localhost:1235" --inputs="$file"
-#done
+    python3 load_tweets.py --db="postgresql://postgres:pass@localhost:1235" --inputs="$file"
+done
 
 echo 'load denormalized'
 for file in $files; do
